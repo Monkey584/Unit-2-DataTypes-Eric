27026538611ca5f998c38b049b995ca284d819b3
Monkey584 <Ericj62887@gmail.com>
2026-09-23 10:17:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 y = float(3)
 print(x,y) """
 
+
 """ values = [1,2.23,5,7,2,30,15]
 print(values)
 for i in values:
@@ -10,6 +11,7 @@ for i in values:
 print(values[0])
 print(values[6]) """
 
+
 """ x = "this is a thing"
 y= x.split( )
 z = y[0]
@@ -17,11 +19,13 @@ print(y)
 print(z)
  """
 
+
 """ day_of_week = input("what day is it? ")
 if day_of_week == "Friday":
     print("correct")
 else:
     print("incorrect") """
+
 
 """ def count_words(sentance):
     words = sentance.split()
@@ -31,8 +35,10 @@ user_sentance = input()
 word_count = count_words(user_sentance)
 print(f"{word_count}") """
 
+
 """ bill = input("How much was the bill")
 print(int(bill) + 20) """
+
 
 """ def Number(value):
     return int(value)
@@ -53,15 +59,22 @@ else:
 x = int(input("enter a number: "))
 print(f"Your number is {number(x)}.") """
 
-def service(x):
+
+""" def service(x):
     if x == "great":
         return "25%"
     elif x == "good":
         return "20%"
     elif x == "okay":
         return "15%"
-    else:
+    elif x == "bad":
         return "0%"
+    else:
+        return "invalid"
 
 x = input("How was the service: ")
-print(service(x))
+print(f"{service(x)}") """
+
+
+def factor(x):
+    x % x == 0
